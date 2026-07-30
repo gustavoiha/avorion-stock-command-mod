c4de6069d75aa7a99496374fd5ea1e8345eb004a
Gustavo Iha <gustavo.iha@reaktor.com>
2026-07-30 12:28:08 +0100

    Bump version to 1.1 in modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -24,7 +24,7 @@ meta =
     authors = {"Gugs_Iha"},
 
     -- Version (major.minor[.patch]).
-    version = "1.0",
+    version = "1.1",
 
     -- Dependencies. Left empty on purpose so the mod isn't tied to a specific
     -- Avorion patch version.
